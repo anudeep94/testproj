65eaa924f104357fc0e3e53f8ebd66fb6a2527a1
vm mac <vmmac@vms-mac.local>
2016-07-07 14:47:06 +0530

    Initial Commit

---
+++ GetStrongTryout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GetStrongTryout
+//
+//  Created by vm mac on 07/07/2016.
+//  Copyright © 2016 PytenLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GetStrongTryout/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GetStrongTryout
+//
+//  Created by vm mac on 07/07/2016.
+//  Copyright © 2016 PytenLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
